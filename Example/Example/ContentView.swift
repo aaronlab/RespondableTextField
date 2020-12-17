e7e4dc8ed5f922b69b4f8bf12952b68d7ddaec06
Aaron Lee <aaronlab.net@gmail.com>
2020-12-17 22:16:54 +0900

    feat: Init Example

---
+++ Example/Example/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Example
+//
+//  Created by Aaron Lee on 2020-12-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
